7853a5018831bef7b3fd09a1bdbf51ce174bfcc4
Daniel Patnode <dbpatnode@gmail.com>
2022-07-06 13:31:57 -0700

    todo using useRef, useReducer and useCallback hooks

--- react-typscript/src/App.tsx
+++ react-typscript/src/App.tsx
@@ -1,12 +1,21 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, useReducer, useRef } from "react";
 import Heading from "./Heading";
 import Box from "./Box";
 import List from "./List";
 import "./App.css";
+import { type } from "os";
 
 interface Payload {
   text: string;
 }
+interface Todo {
+  id: number;
+  done: boolean;
+  text: string;
+}
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
 
 function App() {
   const onListClick = useCallback((item: string) => {
@@ -25,6 +34,34 @@ function App() {
       });
   }, []);
 
+  // useReducer
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+            done: false,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }, []);
+
+  // useRef
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      });
+    }
+  }, []);
   return (
     <div>
       <Heading title='This is the title' />
@@ -32,6 +69,17 @@ function App() {
 
       <Box>{JSON.stringify(payload)}</Box>
       <List items={["suck", "it", "Trebeck"]} onClick={onListClick} />
+      <Heading title='Todos' />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+            Remove
+          </button>
+        </div>
+      ))}
+      <input type='text' ref={newTodoRef} />
+      <button onClick={onAddTodo}>Add Todo</button>
     </div>
   );
 }
