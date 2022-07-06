7156911ee0fe4e5565a85dcc399a6579da07fc13
Daniel Patnode <dbpatnode@gmail.com>
2022-07-06 12:06:56 -0700

    useState with typescript

--- react-typscript/src/App.tsx
+++ react-typscript/src/App.tsx
@@ -1,18 +1,36 @@
-import React, { useCallback } from "react";
+import { useCallback, useState, useEffect } from "react";
 import Heading from "./Heading";
 import Box from "./Box";
 import List from "./List";
 import "./App.css";
+
+interface Payload {
+  text: string;
+}
 
 function App() {
   const onListClick = useCallback((item: string) => {
     alert(item);
   }, []);
 
+  // how to write useState with typescript
+  // the | allows us to take in the payload or null
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPayload(data);
+      });
+  }, []);
+
   return (
     <div>
       <Heading title='This is the title' />
       <Box>Suck it Trebeck</Box>
+
+      <Box>{JSON.stringify(payload)}</Box>
       <List items={["suck", "it", "Trebeck"]} onClick={onListClick} />
     </div>
   );
